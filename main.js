f1d3a980efe226e12d7667a57982119eea257f27
taku0708 <78.tk.nghr@gmail.com>
2021-05-08 13:11:08 +0900

    11

--- main.js
+++ main.js
@@ -119,6 +119,15 @@ english.addEventListener("click", () => {
   blackBlock3.remove();
 });
 hobby.addEventListener("click", () => {
+  // 共通処理
+  menuContent.style.visibility = "hidden";
+  menuIcon.classList.remove("menu_open");
+  imageColorBox1.classList.remove("bgkColor1");
+  imageColorBox2.classList.remove("bgkColor2");
+  imageColorBox3.classList.remove("bgkColor3");
+  // 手書き画像表示のON/OFF
+  lastName.style.visibility = "hidden";
+  firstName.style.visibility = "hidden";
   //   乱数画像表示
   coffeeBox.classList.add("image1");
   clothBox.classList.add("image2");
@@ -140,96 +149,26 @@ hobby.addEventListener("click", () => {
   clothBox.src = cloth[clothNo];
   saunaBox.src = sauna[saunaNo];
   cyclingBox.src = cycling[cyclingNo];
-  imageColorBox3.append(coffeeContent);
-  imageColorBox3.append(clothBox);
-  imageColorBox3.append(saunaContent);
-  imageColorBox1.append(cyclingBox);
-  imageColorBox1.append(blackBlock1);
-  imageColorBox1.append(blackBlock3);
-  // 趣味/説明
+  // 黒枠
   coffeeContent.classList.add("image1");
   clothContent.classList.add("image2");
   saunaContent.classList.add("image3");
   blackBlock1.classList.add("image1");
   blackBlock3.classList.add("image3");
-  //   imageColorBox1.append(blackBlock);
-  imageColorBox2.append(coffeeBox);
-  imageColorBox2.append(clothContent);
-  imageColorBox2.append(saunaBox);
-  // hobby
+  // hobby title
   clothContent.textContent = "hobby";
   coffeeContent.textContent = "coffee";
   saunaContent.textContent = "old cloth";
   blackBlock1.textContent = "cycling";
   blackBlock3.textContent = "sauna";
-  // 画像が表示されてから処理を実行
-  window.addEventListener("load", () => {
-    // 共通処理
-    menuContent.style.visibility = "hidden";
-    menuIcon.classList.remove("menu_open");
-    imageColorBox1.classList.remove("bgkColor1");
-    imageColorBox2.classList.remove("bgkColor2");
-    imageColorBox3.classList.remove("bgkColor3");
-    // 手書き画像表示のON/OFF
-    lastName.style.visibility = "hidden";
-    firstName.style.visibility = "hidden";
-  });
+  // append
+  imageColorBox1.append(cyclingBox);
+  imageColorBox1.append(blackBlock1);
+  imageColorBox1.append(blackBlock3);
+  imageColorBox2.append(coffeeBox);
+  imageColorBox2.append(clothContent);
+  imageColorBox2.append(saunaBox);
+  imageColorBox3.append(coffeeContent);
+  imageColorBox3.append(clothBox);
+  imageColorBox3.append(saunaContent);
 });
-
-// const removeImage = () => {
-//   coffeeBox.remove();
-//   clothBox.remove();
-//   saunaBox.remove();
-//   cyclingBox.remove();
-//   coffeeContent.remove();
-//   clothContent.remove();
-//   saunaContent.remove();
-//   blackBlock1.remove();
-//   blackBlock3.remove();
-// };
-
-// const commonAction = () => {
-//   menuContent.style.visibility = "hidden";
-//   menuIcon.classList.remove("menu_open");
-//   if (element.classList.contains("bgkcolor") == true) {
-//     imageColorBox1.classList.remove("bgkColor1");
-//     imageColorBox2.classList.remove("bgkColor2");
-//     imageColorBox3.classList.remove("bgkColor3");
-//   } else {
-//     imageColorBox1.classList.add("bgkColor1");
-//     imageColorBox2.classList.add("bgkColor2");
-//     imageColorBox3.classList.add("bgkColor3");
-//   }
-// };
-
-// const changeLine = () => {
-//   if (english.onclick) {
-//     line1.textContent = "Geeksalon/webEx/10th";
-//     line2.textContent = "Born in 1999, Living in Tokyo";
-//     line3.textContent = "grew up in Tokunoshima-island in childhood";
-//     line4.textContent =
-//       "I study Hindhi in Tokyo University of foreign studies. Currently, I research the Politics of Africa.";
-//     line5.textContent =
-//       "However, to be frank with you, I don't like studying Hindhi, more to say, the languages.";
-//     line6.textContent =
-//       " Now, I'm taking a year off from the university to work in the company as a engineer.";
-//     line7.textContent =
-//       "so hard, so busy, so difficult, but more than that, I feel so happy";
-//     lastName.style.visibility = "hidden";
-//     firstName.style.visibility = "hidden";
-//   } else if (hindhi.onclick) {
-//     line1.textContent = "";
-//     line2.textContent = "";
-//     line3.textContent = "";
-//     line4.textContent =
-//       "मैं टोक्यो यूनिवर्सिटी ऑफ फॉरेन स्टडीज में हिंदियों का अध्ययन करता हूं।  वर्तमान में, मैं अफ्रीका की राजनीति पर शोध करता हूं।";
-//     line5.textContent =
-//       "हालाँकि, आपके साथ फ्रैंक होने के लिए, मुझे हिंदियों का अध्ययन करना पसंद नहीं है, कहने के लिए, भाषाएँ।";
-//     line6.textContent =
-//       "अब, मैं एक इंजीनियर के रूप में कंपनी में काम करने के लिए विश्वविद्यालय से एक साल की छुट्टी ले रहा हूं।";
-//     line7.textContent =
-//       "इतना कठिन, इतना व्यस्त, इतना कठिन, लेकिन उससे भी अधिक, मैं इतना खुश महसूस करता हूं";
-//     lastName.style.visibility = "visible";
-//     firstName.style.visibility = "visible";
-//   }
-// };
